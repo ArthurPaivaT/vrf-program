fd71645a093926e3f93cf793af97cf850c47aaec
Arthur Tavares <arthur001123@gmail.com>
2022-12-04 14:28:32 -0300

    Add PDA structure for hashes and results storage

---
+++ programs/vrf-program/src/state/random_value.rs
@@ -0,0 +1,21 @@
+use anchor_lang::prelude::*;
+
+/// whitelists are used to control what gems can/can't go into the vault
+/// currently 2 types of vault lists are supported: by mint and by creator
+/// if the whitelist PDA exists, then the mint/creator is considered accepted
+/// if at least 1 whitelist PDA exists total, then all deposit attempts will start getting checked
+#[repr(C)]
+#[account]
+pub struct RandomValue {
+    pub hash: PubKey,
+    pub min: u64,
+    pub max: u64,
+    pub result: u64,
+    pub processed: bool,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("User doesn't have tickets")]
+    WinnerNotValid,
+}
