ea408363e3ea341ff031ec662a95eddfc515f95a
Arthur Tavares <arthur001123@gmail.com>
2022-12-19 21:49:42 -0300

    Update dependencies and fix imports

--- programs/vrf-program/src/instructions/commit.rs
+++ programs/vrf-program/src/instructions/commit.rs
@@ -1,22 +1,19 @@
 use anchor_lang::prelude::*;
 use anchor_lang::Accounts;
-use solana_program::program::invoke;
-use solana_program::system_instruction;
 
 use crate::state::random_value;
 
-#[derive(Accounts)]
+#[derive(Accounts)] // TODO transfer some sol to the revealer on commit
 pub struct Commit<'info> {
     #[account(init, payer = user, space = 8 + std::mem::size_of::<random_value::RandomValue>())]
     pub random_value: Account<'info, random_value::RandomValue>,
-    #[account(mut)]
-    pub hash: Account<'info>,
     pub system_program: Program<'info, System>,
+    #[account(mut)]
+    pub user: Signer<'info>,
 }
 
 pub fn handler(ctx: Context<Commit>, min: u64, max: u64) -> Result<()> {
     let random_value = &mut ctx.accounts.random_value;
-    random_value.hash = ctx.accounts.hash.key();
     random_value.processed = false;
     random_value.result = 0;
     random_value.min = min;
