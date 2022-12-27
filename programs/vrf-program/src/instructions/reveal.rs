4162679c9aceec02c628d1b66a6051ac5ef15e47
Arthur Tavares <arthur001123@gmail.com>
2022-12-27 17:12:18 -0300

    Verify if reveal signer account is the set revealer

--- programs/vrf-program/src/instructions/reveal.rs
+++ programs/vrf-program/src/instructions/reveal.rs
@@ -1,12 +1,17 @@
 use anchor_lang::prelude::*;
 use anchor_lang::Accounts;
+use std::str::FromStr;
 
 use crate::state::random_value;
+
+pub const REVEALER: &str = "2xhBxVVuXkdq2MRKerE9mr2s1szfHSedy21MVqf8gPoM";
 
 #[derive(Accounts)]
 pub struct Reveal<'info> {
     #[account()]
     pub random_value: Account<'info, random_value::RandomValue>,
+    #[account(mut, address = Pubkey::from_str(REVEALER).unwrap())]
+    pub revealer: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
